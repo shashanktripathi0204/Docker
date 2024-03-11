d17c210d021848fdd8c44dbc76da392bc9a65c07
Shashank Tripathi <shashank.tripathi4@ibm.com>
2024-03-12 01:11:44 +0530

    checking the pull request rule 02

--- first-demo-starting/app.mjs
+++ first-demo-starting/app.mjs
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 
 await connectToDatabase();
 
-// applying comment to check pull request rule
+// applying comment to check pull request rule, again
 app.listen(3000);
